ff41fef2cfc0e3893a42ad95b4cae3d598f3d5a2
Michael Leditschke <mike@ammd.com.au>
2017-02-24 14:39:46 +1100

    Add convenience methods for stack management
    Methods mirror those used for template creation

---
+++ aws/manageSolutionStack.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [[ -n "${GENERATION_DEBUG}" ]]; then set ${GENERATION_DEBUG}; fi
+trap 'exit ${RESULT:-1}' EXIT SIGHUP SIGINT SIGTERM
+
+${GENERATION_DIR}/manageStack.sh -t solution "$@"
+RESULT=$?
+
