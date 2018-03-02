909976ae296cc8283e471b2a6f36d91f73c57815
roleyfoley <roleyfoley@gmail.com>
2018-03-02 13:39:19 +1100

    Move Snaphost details to name from ARN

--- aws/utility.sh
+++ aws/utility.sh
@@ -693,13 +693,8 @@ function delete_oai_credentials() {
 function create_snapshot() { 
   local region="$1"; shift
   local db_identifier="$1"; shift
-  local snapshot_suffix="$1"; shift
+  local db_snapshot_identifier="$1"; shift
   
-  db_snapshot_identifier="${db_identifier}-$(date -u +%Y-%m-%d-%H-%M-%S)"
-  if [[ -n "${snapshot_suffix}" ]]; then
-    db_snapshot_identifier="${db_snapshot_identifier}-${snapshot_suffix}"
-  fi
-
   # Check that the database exists
   db_info=$(aws --region "${region}" rds describe-db-instances --db-instance-identifier ${db_identifier} )
 
@@ -708,10 +703,9 @@ function create_snapshot() {
     aws --region "${region}" rds wait db-snapshot-available --db-snapshot-identifier "${db_snapshot_identifier}"  || return $?
     db_snapshot=$(aws --region "${region}" rds describe-db-snapshots --db-snapshot-identifier "${db_snapshot_identifier}" || return $?)
   fi
-  echo -n "$(echo "${db_snapshot}" | jq -r '.DBSnapshots[0].DBSnapshotArn' )" 
-  
-  return 0
+  echo -n "$(echo "${db_snapshot}" | jq -r '.DBSnapshots[0] | .DBSnapshotIdentifier + " " + .SnapshotCreateTime' )" 
 }
+
 
 # -- Git Repo Management -- 
 function clone_git_repo() {
