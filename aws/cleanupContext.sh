7c905a3a47e46b15362447b1aa4e46de3359b527
Michael Leditschke <mike@ammd.com.au>
2019-07-29 10:49:11 +1000

    <fix> Use log level in cleanup logic (#920)
    Cleanup logic did not retain temporary files unless GENERATION_DEBUG
    was set.
    
    Switch to using willLog which will still honour GENERATION_DEBUG
    but will also honour GENERATION_LOG_LEVEL=debug

--- aws/cleanupContext.sh
+++ aws/cleanupContext.sh
@@ -5,8 +5,7 @@
 # Context cleanup is only done from the script that set the context
 [[ -z "${GENERATION_CONTEXT_DEFINED_LOCAL}" ]] && return 0
 
-if [[ -z "${GENERATION_DEBUG}" ]]; then
-  [[ -n "${GENERATION_TMPDIR}" ]] && rm -rf "${GENERATION_TMPDIR}"
-fi
+! willLog "debug" &&
+    [[ -n "${GENERATION_TMPDIR}" ]] && rm -rf "${GENERATION_TMPDIR}"
 
 return 0
