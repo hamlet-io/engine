77ee01502c1cec369815bc80bead9e3ac2a98f78
rossmurr4y <ross.murray@protonmail.com>
2019-09-27 16:48:58 -0700

    Allow for loading of a plugin utility script.

--- aws/common.sh
+++ aws/common.sh
@@ -7,6 +7,11 @@
 . ${GENERATION_DIR}/utility.sh
 . ${GENERATION_DIR}/contextTree.sh
 
+# Load plugin utilities if they exist
+if [[ -f ${GENERATION_PLUGIN_DIR}/utility.sh]]; then
+  . ${GENERATION_PLUGIN_DIR}/utility.sh
+fi
+
 function getLogLevel() {
   checkLogLevel "${GENERATION_LOG_LEVEL}"
 }
