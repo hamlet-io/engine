4d25f6a3eea14eafed714bde06e90209928ca270
roleyfoley <roleyfoley@gmail.com>
2019-11-08 13:43:47 +1100

    <refactor> Remove redundant or unused scripts
    - Removes add* scripts which have now been replaced with the cookie cutter patterns approach
    - Removes the create*Level* templates which provided a minimal shortcut to adding a parameter
    - Removes some gen2-gen3 conversion scripts

--- aws/createAccountTemplate.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-[[ -n "${GENERATION_DEBUG}" ]] && set ${GENERATION_DEBUG}
-trap 'exit ${RESULT:-1}' EXIT SIGHUP SIGINT SIGTERM
-
-${GENERATION_DIR}/createTemplate.sh -l account "$@"
-RESULT=$?
