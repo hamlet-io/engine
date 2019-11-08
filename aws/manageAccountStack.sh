5318fb532ec62ea2a12eeb7a1a250158137d3034
roleyfoley <roleyfoley@gmail.com>
2019-11-08 13:43:47 +1100

    <refactor> Remove redundant or unused scripts
    - Removes add* scripts which have now been replaced with the cookie cutter patterns approach
    - Removes the create*Level* templates which provided a minimal shortcut to adding a parameter
    - Removes some gen2-gen3 conversion scripts

--- aws/manageAccountStack.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-[[ -n "${GENERATION_DEBUG}" ]] && set ${GENERATION_DEBUG}
-trap 'exit ${RESULT:-1}' EXIT SIGHUP SIGINT SIGTERM
-
-${GENERATION_DIR}/manageStack.sh -t account "$@"
-RESULT=$?
-
