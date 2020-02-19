e151af88e75bc0923a604457b246856fe0a17dd3
Michael Foley <roleyfoley@gmail.com>
2020-02-19 21:40:58 +1100

    <feature> jenkins docker build (#1243)
    * <feature> Jenkins trigger docker build
    
    * synatx cleanup
    
    * align jenkins task with repo
    
    * fix agent label
    
    * fix test dir creation
    
    * fix up testing
    
    * move agent step to align with tests

--- Jenkinsfile
+++ Jenkinsfile
@@ -5,21 +5,34 @@ pipeline {
         timestamps()
     }
 
-    agent {
-        label 'codeontaplatest'
-    }
-
-    environment {
-        GENERATION_DIR="${WORKSPACE}/aws"
-        GENERATION_BASE_DIR="${WORKSPACE}"
-    }
+    agent none
 
     stages {
         stage('Run AWS Template Tests') {
+            agent {
+                label 'codeontaplatest'
+            }
+            environment {
+                GENERATION_DIR="${WORKSPACE}/aws"
+                GENERATION_BASE_DIR="${WORKSPACE}"
+            }
             steps {
                 sh '''#!/usr/bin/env bash
                 ${WORKSPACE}/test/aws/run_aws_template_tests.sh
                 '''
+            }
+        }
+
+        stage('Trigger Docker Build') {
+            when {
+                branch 'master'
+                beforeAgent true
+            }
+            agent none
+            steps {
+                build (
+                    job: '../docker-gen3/master'
+                )
             }
         }
     }
--- test/aws/run_aws_template_tests.sh
+++ test/aws/run_aws_template_tests.sh
@@ -4,19 +4,29 @@ echo "###############################################"
 echo "# Running template tests for the AWS provider #"
 echo "###############################################"
 
+TEST_OUTPUT_DIR="${TEST_OUTPUT_DIR:-"./cot_tests"}"
+
+if [[ -d "${TEST_OUTPUT_DIR}" ]]; then
+    rm -r "${TEST_OUTPUT_DIR}"
+    mrkdir "${TEST_OUTPUT_DIR}"
+else
+    mkdir -p "${TEST_OUTPUT_DIR}"
+fi
+
+echo "Output Dir: ${TEST_OUTPUT_DIR}"
 echo "Generating unit list..."
-${GENERATION_DIR}/createTemplate.sh -i mock -p aws -p awstest -o ~/cot_tests/ -l unitlist > /dev/null 2>&1
-UNIT_LIST="$(jq -r '.DeploymentUnits | join(" ")' < ~/cot_tests/unitlistconfig.json)"
+${GENERATION_DIR}/createTemplate.sh -i mock -p aws -p awstest -o "${TEST_OUTPUT_DIR}" -l unitlist
+UNIT_LIST="$(jq -r '.DeploymentUnits | join(" ")' < "${TEST_OUTPUT_DIR}/unitlistconfig.json")"
 
 for unit in $UNIT_LIST; do
     echo "Creating templates for $unit ..."
-    ${GENERATION_DIR}/createTemplate.sh -i mock -p aws -p awstest -o ~/cot_tests/ -l segment -u $unit > /dev/null 2>&1 || true
-    ${GENERATION_DIR}/createTemplate.sh -i mock -p aws -p awstest -o ~/cot_tests/ -l solution -u $unit > /dev/null 2>&1 || true
-    ${GENERATION_DIR}/createTemplate.sh -i mock -p aws -p awstest -o ~/cot_tests/ -l application -u $unit > /dev/null 2>&1 || true
+    ${GENERATION_DIR}/createTemplate.sh -i mock -p aws -p awstest -o "${TEST_OUTPUT_DIR}" -l segment -u $unit > /dev/null 2>&1 || true
+    ${GENERATION_DIR}/createTemplate.sh -i mock -p aws -p awstest -o "${TEST_OUTPUT_DIR}" -l solution -u $unit > /dev/null 2>&1 || true
+    ${GENERATION_DIR}/createTemplate.sh -i mock -p aws -p awstest -o "${TEST_OUTPUT_DIR}" -l application -u $unit > /dev/null 2>&1 || true
 done
 
-cot test generate --directory ~/cot_tests/ -o ~/cot_tests/test_templates.py
+cot test generate --directory "${TEST_OUTPUT_DIR}" -o "${TEST_OUTPUT_DIR}/test_templates.py"
 
-cd ~/cot_tests
+cd "${TEST_OUTPUT_DIR}"
 echo "Running Tests..."
-cot test run -t ~/cot_tests/test_templates.py
+cot test run -t "./test_templates.py"
