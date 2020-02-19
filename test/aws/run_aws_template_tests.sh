6f6f54ca5acb55972104461a47317bc7ec2867c2
roleyfoley <roleyfoley@gmail.com>
2020-02-19 20:52:43 +1100

    fix test dir creation

--- test/aws/run_aws_template_tests.sh
+++ test/aws/run_aws_template_tests.sh
@@ -4,19 +4,26 @@ echo "###############################################"
 echo "# Running template tests for the AWS provider #"
 echo "###############################################"
 
+OUTPUT_DIR="${OUTPUT_DIR:-"./cot_tests"}"
+
+if [[ ! -d "${OUTPUT_DIR}" ]]; then
+    mkdir -p "${OUTPUT_DIR}"
+fi
+
+echo "Output Dir: ${OUTPUT_DIR}"
 echo "Generating unit list..."
-${GENERATION_DIR}/createTemplate.sh -i mock -p aws -p awstest -o ~/cot_tests/ -l unitlist > /dev/null 2>&1
-UNIT_LIST="$(jq -r '.DeploymentUnits | join(" ")' < ~/cot_tests/unitlistconfig.json)"
+${GENERATION_DIR}/createTemplate.sh -i mock -p aws -p awstest -o "${OUTPUT_DIR}" -l unitlist > /dev/null 2>&1
+UNIT_LIST="$(jq -r '.DeploymentUnits | join(" ")' < "${OUTPUT_DIR}/unitlistconfig.json")"
 
 for unit in $UNIT_LIST; do
     echo "Creating templates for $unit ..."
-    ${GENERATION_DIR}/createTemplate.sh -i mock -p aws -p awstest -o ~/cot_tests/ -l segment -u $unit > /dev/null 2>&1 || true
-    ${GENERATION_DIR}/createTemplate.sh -i mock -p aws -p awstest -o ~/cot_tests/ -l solution -u $unit > /dev/null 2>&1 || true
-    ${GENERATION_DIR}/createTemplate.sh -i mock -p aws -p awstest -o ~/cot_tests/ -l application -u $unit > /dev/null 2>&1 || true
+    ${GENERATION_DIR}/createTemplate.sh -i mock -p aws -p awstest -o "${OUTPUT_DIR}" -l segment -u $unit > /dev/null 2>&1 || true
+    ${GENERATION_DIR}/createTemplate.sh -i mock -p aws -p awstest -o "${OUTPUT_DIR}" -l solution -u $unit > /dev/null 2>&1 || true
+    ${GENERATION_DIR}/createTemplate.sh -i mock -p aws -p awstest -o "${OUTPUT_DIR}" -l application -u $unit > /dev/null 2>&1 || true
 done
 
-cot test generate --directory ~/cot_tests/ -o ~/cot_tests/test_templates.py
+cot test generate --directory "${OUTPUT_DIR}" -o "${OUTPUT_DIR}/test_templates.py"
 
-cd ~/cot_tests
+cd "${OUTPUT_DIR}"
 echo "Running Tests..."
-cot test run -t ~/cot_tests/test_templates.py
+cot test run -t "${OUTPUT_DIR}/test_templates.py"
