34de73499980df0a884327cce3c566c081a1143d
roleyfoley <roleyfoley@gmail.com>
2020-02-19 21:30:34 +1100

    fix up testing

--- test/aws/run_aws_template_tests.sh
+++ test/aws/run_aws_template_tests.sh
@@ -4,26 +4,29 @@ echo "###############################################"
 echo "# Running template tests for the AWS provider #"
 echo "###############################################"
 
-OUTPUT_DIR="${OUTPUT_DIR:-"./cot_tests"}"
+TEST_OUTPUT_DIR="${TEST_OUTPUT_DIR:-"./cot_tests"}"
 
-if [[ ! -d "${OUTPUT_DIR}" ]]; then
-    mkdir -p "${OUTPUT_DIR}"
+if [[ -d "${TEST_OUTPUT_DIR}" ]]; then
+    rm -r "${TEST_OUTPUT_DIR}"
+    mrkdir "${TEST_OUTPUT_DIR}"
+else
+    mkdir -p "${TEST_OUTPUT_DIR}"
 fi
 
-echo "Output Dir: ${OUTPUT_DIR}"
+echo "Output Dir: ${TEST_OUTPUT_DIR}"
 echo "Generating unit list..."
-${GENERATION_DIR}/createTemplate.sh -i mock -p aws -p awstest -o "${OUTPUT_DIR}" -l unitlist > /dev/null 2>&1
-UNIT_LIST="$(jq -r '.DeploymentUnits | join(" ")' < "${OUTPUT_DIR}/unitlistconfig.json")"
+${GENERATION_DIR}/createTemplate.sh -i mock -p aws -p awstest -o "${TEST_OUTPUT_DIR}" -l unitlist
+UNIT_LIST="$(jq -r '.DeploymentUnits | join(" ")' < "${TEST_OUTPUT_DIR}/unitlistconfig.json")"
 
 for unit in $UNIT_LIST; do
     echo "Creating templates for $unit ..."
-    ${GENERATION_DIR}/createTemplate.sh -i mock -p aws -p awstest -o "${OUTPUT_DIR}" -l segment -u $unit > /dev/null 2>&1 || true
-    ${GENERATION_DIR}/createTemplate.sh -i mock -p aws -p awstest -o "${OUTPUT_DIR}" -l solution -u $unit > /dev/null 2>&1 || true
-    ${GENERATION_DIR}/createTemplate.sh -i mock -p aws -p awstest -o "${OUTPUT_DIR}" -l application -u $unit > /dev/null 2>&1 || true
+    ${GENERATION_DIR}/createTemplate.sh -i mock -p aws -p awstest -o "${TEST_OUTPUT_DIR}" -l segment -u $unit > /dev/null 2>&1 || true
+    ${GENERATION_DIR}/createTemplate.sh -i mock -p aws -p awstest -o "${TEST_OUTPUT_DIR}" -l solution -u $unit > /dev/null 2>&1 || true
+    ${GENERATION_DIR}/createTemplate.sh -i mock -p aws -p awstest -o "${TEST_OUTPUT_DIR}" -l application -u $unit > /dev/null 2>&1 || true
 done
 
-cot test generate --directory "${OUTPUT_DIR}" -o "${OUTPUT_DIR}/test_templates.py"
+cot test generate --directory "${TEST_OUTPUT_DIR}" -o "${TEST_OUTPUT_DIR}/test_templates.py"
 
-cd "${OUTPUT_DIR}"
+cd "${TEST_OUTPUT_DIR}"
 echo "Running Tests..."
-cot test run -t "${OUTPUT_DIR}/test_templates.py"
+cot test run -t "./test_templates.py"
